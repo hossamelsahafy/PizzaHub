a7d58a8261d0cc344f968feafead566ec9ee56ee
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-08-26 21:28:36 +0300

    Files

--- Frontend/src/api/appitizersApi.ts
+++ Frontend/src/api/appitizersApi.ts
@@ -1,5 +1,5 @@
 export const fetchAppitizer = async () => {
-    const res = await fetch('http://localhost:5000/appitizer');
+    const res = await fetch('http://64.227.119.208:5000/appitizer');
     const data = await res.json();    
     return data;
 }
