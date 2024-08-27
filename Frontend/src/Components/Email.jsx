71edd2ccc7a26031bc944e5cfdc9c058d5ed3a19
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-08-27 20:22:11 +0300

    files

--- Frontend/src/Components/Email.jsx
+++ Frontend/src/Components/Email.jsx
@@ -1,51 +1,53 @@
-import { useState, Fragment, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './style/email.css';
 import axios from 'axios';
-import success from '../assets/Images/Covers/sucess.webp'
+import success from '../assets/Images/Covers/sucess.webp';
 
 const Email = () => {
     const [validUrl, setValidUrl] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
     const { id, token } = useParams();
 
     useEffect(() => {
         const verifyEmail = async () => {
             try {
-                const url = `https://pizzahub.me:5000/users/${id}/verify/${token}`;
+                const url = `https://pizzahub.me/users/${id}/verify/${token}`;
                 const { status } = await axios.get(url);
                 
                 if (status === 200) {
                     setValidUrl(true);
                 } else {
                     setValidUrl(false);
+                    setError(true);
                 }
             } catch (error) {
-                if (error.response) {
-                    console.error('Error response:', error.response.data);
-                    console.error('Status code:', error.response.status);
-                    console.error('Headers:', error.response.headers);
-                } else if (error.request) {
-                    console.error('No response received:', error.request);
-                } else {
-                    console.error('Error during request setup:', error.message);
-                }
+                console.error('Error verifying email:', error);
                 setValidUrl(false);
+                setError(true);
+            } finally {
+                setLoading(false);
             }
         };
         verifyEmail();
     }, [id, token]);
 
+    if (loading) {
+        return <h2>Loading...</h2>; // Display loading state
+    }
+
     return (
-        <Fragment>
+        <>
             {validUrl ? (
                 <div className="containerEmail">
                     <h3>Email Verified Successfully, Please Exit The Page And Login!</h3>
-                    <img src={success} alt="Logo" />
+                    <img src={success} alt="Success" />
                 </div>
             ) : (
-                <h2>Error 404 Not Found</h2>
+                <h2>{error ? 'There was an error verifying your email. Please check the link and try again.' : 'Error 404 Not Found'}</h2>
             )}
-        </Fragment>
+        </>
     );
 };
 
