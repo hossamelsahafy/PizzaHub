3c111742b32749298dac3af80b91c8a63ba16908
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-08-27 07:22:30 +0300

    Files

--- Frontend/src/api/appitizersApi.ts
+++ Frontend/src/api/appitizersApi.ts
@@ -1,6 +1,6 @@
 export const fetchAppitizer = async () => {
     try {
-        const res = await fetch('https://64.227.119.208:5000/appitizer');
+        const res = await fetch('https://pizzahub.me:5000/appitizer');
         if (!res.ok) {
             throw new Error(`HTTP error! status: ${res.status}`);
         }
@@ -8,6 +8,6 @@ export const fetchAppitizer = async () => {
         return data;
     } catch (error) {
         console.error('Failed to fetch appetizer:', error);
-        return null;  // comment
+        return null;
     }
 }
